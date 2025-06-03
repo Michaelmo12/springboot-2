3284d621c2ed8ddb042b070386b9d5df7c7e1150
Michael <mordohmichael9@gmail.com>
2025-06-03 23:18:40 +0300

    Project SpringBoot advanced Engineering System

---
+++ src/test/java/org/example/stage1/Stage1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.stage1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Stage1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
